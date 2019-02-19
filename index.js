278237c2b03a829575cc909a9744d57361d6afe4
pradnya <patilpradnya1193@gmail.com>
2019-02-19 19:13:37 +0530

    :lipstick: :sparkles: computer move and reset button added

--- index.js
+++ index.js
@@ -77,15 +77,166 @@ function onBoxClick() {
     grid[colIdx][rowIdx] = newValue;
     renderMainGrid();
     addClickHandlers();
+    winner();
+    setTimeout(function() {
+        computerMove();
+    }, 300);
 }
 
 function addClickHandlers() {
     var boxes = document.getElementsByClassName("box");
     for (var idx = 0; idx < boxes.length; idx++) {
-        boxes[idx].addEventListener('click', onBoxClick, false);
+        if(boxes[idx].textContent === "") {
+            boxes[idx].addEventListener('click', onBoxClick, false);
+        }
     }
+}
+
+function computerMove() {
+    console.log('computermove');
+    var emptycells = [],
+    gridval = [],
+    cordinate = [];
+    for(let colIdx=0; colIdx < GRID_LENGTH ; colIdx++ ) {
+        for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
+            const value = grid[colIdx][rowIdx];
+            if(value === 0) {
+                rowid = rowIdx;
+                colid = colIdx;
+                gridval = [colid, rowid];
+                emptycells = emptycells.concat([gridval]);
+                console.log('emptycells', emptycells);
+            }
+        }
+    }
+    cordinate = emptycells[Math.floor(Math.random() * (emptycells.length))];
+    newcolid = cordinate[0];
+    newrowid = cordinate[1];
+    let newValue = 2;
+    grid[newcolid][newrowid] = newValue;
+    renderMainGrid();
+    addClickHandlers();
+    winner();
+}
+
+function resetGame() {
+    var boxes = document.getElementsByClassName("box");
+    for (var idx = 0; idx < boxes.length; idx++) {
+        boxes[idx].textContent == "";
+        if(boxes[idx].children[0]) {
+            boxes[idx].children[0].remove();
+        }
+    }
+    for(let colIdx=0; colIdx < GRID_LENGTH ; colIdx++ ) {
+        for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
+            grid[colIdx][rowIdx] = 0;
+        }
+    }
+
+    document.getElementsByClassName("result")[0].innerHTML = '';
+}
+
+function resethandler() {
+    document.getElementsByClassName("resetbtn")[0].addEventListener('click', resetGame, false);
+}
+
+function winner() {
+    var boxes = document.getElementsByClassName("box"),
+        winflag = 0,
+        cross = '';
+    if(boxes[0].textContent !== '' && boxes[0].textContent === boxes[1].textContent && boxes[0].textContent === boxes[2].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[0].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[0].textContent !== '' && boxes[0].textContent === boxes[3].textContent && boxes[0].textContent === boxes[6].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[6].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[0].textContent !== '' && boxes[0].textContent === boxes[4].textContent && boxes[0].textContent === boxes[8].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[4].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[1].textContent !== '' && boxes[1].textContent === boxes[4].textContent && boxes[1].textContent === boxes[7].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[1].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[2].textContent !== '' && boxes[2].textContent === boxes[5].textContent && boxes[2].textContent === boxes[8].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[2].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[3].textContent !== '' && boxes[3].textContent === boxes[4].textContent && boxes[3].textContent === boxes[5].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[3].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[6].textContent !== '' && boxes[6].textContent === boxes[7].textContent && boxes[6].textContent === boxes[8].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[6].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+
+    if(boxes[2].textContent !== '' && boxes[2].textContent === boxes[4].textContent && boxes[2].textContent === boxes[6].textContent) {
+        winflag = 1;
+        console.log('win');
+        if(boxes[2].textContent === 'X'){
+            cross = 'X';
+        } else {
+            cross = 'O'
+        }
+    }
+    console.log(winflag, cross);
+    if(winflag == 1 && cross == "X") {
+        console.log('won');
+        document.getElementsByClassName("result")[0].innerHTML = 'You Won!!!';
+    } else if(cross == "O") {
+        console.log('lost');
+        document.getElementsByClassName("result")[0].innerHTML = 'You lost!!!';
+    } else {
+        document.getElementsByClassName("result")[0].innerHTML = '';
+    }
+    
 }
 
 initializeGrid();
 renderMainGrid();
 addClickHandlers();
+resethandler();
